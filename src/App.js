160a48e382dfc59a4c4f80ea41e7972716398b67
comptyadel96 <adoulation2007@hotmail.com>
2022-10-17 12:03:01 +0200

    v3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar"
 import { FiMail, FiPhone } from "react-icons/fi"
-import { ImLocation2 } from "react-icons/im"
+import { ImLocation2, ImInstagram } from "react-icons/im"
+import { MdOutlineFacebook } from "react-icons/md"
 import SlideShow from "./components/SlideShow"
 
 function App() {
@@ -8,57 +9,55 @@ function App() {
     <div className="bg-gray-100 h-full flex flex-col">
       {/* navbar */}
       <Navbar />
-      {/* header */}
-      <div className="flex  mx-auto justify-evenly mb-10 bg-white relative">
-        <div className=" w-1/3 bg-gradient-to-br from-emerald-500 to-emerald-300">
-          <p className="text-gray-800 lg:text-4xl flex flex-col items-center bg-white lg:py-5 font-bold">
-            Adara roto
+      {/* company infos */}
+      <div className="flex items-center w-full bg-gray-800 py-1  px-5">
+        <div className="flex items-center mx-3 max-w-fit">
+          <FiMail className="text-white lg:text-4xl text-xl ml-2" />
+          <p className="font-semibold text-lg text-white ml-1">
+            roto@adarasarl.com
           </p>
-          <div className="flex flex-col  w-full bg-gray-800 lg:py-5 px-3">
-            <div className="flex items-center my-1">
-              <FiMail className="text-white text-4xl ml-5" />
-              <p className="font-semibold text-lg text-white ml-5">
-                roto@adarasarl.com
-              </p>
-            </div>
-            <div className="flex items-center my-1">
-              <FiPhone className="text-white text-4xl ml-5" />
-              <p className="font-semibold text-lg text-white ml-5">
-                +213 560 97 52 64
-              </p>
-            </div>
-            <div className="flex items-center my-1">
-              <ImLocation2 className="text-white text-4xl ml-5" />
-              <p className="font-semibold text-lg text-white ml-5">
-                55, Cité Ben Sallah, Oued El Alleug, (W) de Blida{" "}
-              </p>
-            </div>
-          </div>
         </div>
-        {/* <img
-          src="/images/citerne-blanche.jpg"
-          alt="bureau adara roto"
-          className="max-h-[70vh] hue-rotate-90"
-        /> */}
+        <div className="flex items-center mx-3 max-w-fit">
+          <FiPhone className="text-white lg:text-4xl text-xl ml-2" />
+          <p className="font-semibold text-lg text-white ml-1">
+            +213 560 97 52 64
+          </p>
+        </div>
+        <div className="flex items-center mx-3 max-w-fit">
+          <ImLocation2 className="text-white lg:text-4xl text-xl ml-2" />
+          <p className="font-semibold text-lg text-white ml-1">
+            55, Cité Ben Sallah, Oued El Alleug, (W) de Blida{" "}
+          </p>
+        </div>
+        <div className="self-end flex items-center ml-auto">
+          <a
+            href="https://www.facebook.com/profile.php?id=100082864376144"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <MdOutlineFacebook className="text-white text-3xl mx-6 cursor-pointer" />{" "}
+          </a>
+          <ImInstagram className="text-white text-3xl mx-6 cursor-pointer " />
+        </div>
+      </div>
+      {/* header */}
+      <div className="mb-10 bg-white relative w-full">
         <SlideShow />
-        {/* <div className="absolute left-1/2 -translate-x-[50%] bottom-10 px-5 py-3 bg-[rgba(0,0,0,0.5)]">
-        <h1 className="text-3xl text-white">Citerne Vertical <br/></h1>
-        <p className="text-white text-xl">5000 Litres 3 couches Polyethylene</p>
-       </div> */}
       </div>
       {/* body */}
-      <div className="bg-white lg:w-[70%] mx-auto p-4 flex flex-col shadow-md border mb-10">
-        <div className="flex relative">
+      <div className="bg-white lg:w-full lg:py-10 mx-auto p-4 flex flex-col shadow-md border mb-10 relative">
+        <div className=" lg:block hidden absolute top-1/2 -translate-y-1/2 left-0 w-0 h-0 border-t-[66px] border-t-transparent border-b-[74px] border-b-transparent border-l-[140px] border-l-emerald-400" />
+        <div className="flex justify-center flex-wrap relative">
           <img
             src="/images/citernes.png"
             alt="citernes adara"
-            className="lg:max-h-60 border-r-2 border-r-green-500 pr-4"
+            className="lg:max-h-60 max-h-[100%] lg:bg-inherit bg-gray-200  object-cover lg:border-r-2 border-r-green-500 lg:pr-4"
           />
-          <div className=" ml-[10%]">
-            <h1 className="lg:text-4xl font-semibold border-b-2 border-b-green-500 pb-2 max-w-fit mb-3 ">
+          <div className=" lg:ml-[10%]">
+            <h1 className="lg:text-4xl text-3xl  font-semibold border-b-2 border-b-green-500 pb-2 max-w-fit mb-3 ">
               Adara roto c'est quoi ?
             </h1>
-            <p className=" font-semibold max-w-2xl text-lg text-gray-800">
+            <p className=" font-semibold max-w-2xl text-lg text-gray-800 leading-relaxed">
               Adara roto est une entreprise spécialisée dans la vente de
               citernes alimentaires en plastique , spécialement "Polyethylene"
               <br />
@@ -66,15 +65,24 @@ function App() {
             </p>
             {/* couches */}
             <div className="flex mx-auto mt-5 justify-evenly">
-              <div className="flex flex-col items-center">
+              <div className="flex flex-col items-center  p-5">
+                <p className="text-2xl text-gray-500  mb-2 rounded-lg">
+                  Extérieur
+                </p>
                 <div className="w-20 h-11 rounded-full bg-gray-300" />
                 <p className="font-semibold">Gris</p>
               </div>
-              <div className="flex flex-col items-center">
+              <div className="flex flex-col items-center  p-5">
+                <p className="text-2xl text-gray-500  mb-2 rounded-lg">
+                  Centre
+                </p>
                 <div className="w-20 h-11 rounded-full bg-black" />
                 <p className="font-semibold">Noir</p>
               </div>
-              <div className="flex flex-col items-center">
+              <div className="flex flex-col items-center  p-5">
+                <p className="text-2xl text-gray-500  mb-2 rounded-lg">
+                  Intérieur
+                </p>
                 <div className="w-20 h-11 rounded-full bg-white shadow border" />
                 <p className="font-semibold">Blanc</p>
               </div>
--- src/components/SlideShow.js
+++ src/components/SlideShow.js
@@ -18,22 +18,22 @@ function SlideShow() {
     )
   }
   return (
-    <div className="relative flex w-[80%] mx-auto  mb-16 flex-wrap">
+    <div className="relative flex  w-full mx-auto  mb-16 flex-wrap">
       <div className="flex flex-col w-full">
         <img
           src={`${imgArr[currIndex].url}`}
           alt=""
-          className="max-h-[70vh] object-cover mx-auto "
+          className="lg:max-w-[70rem] max-w-[100%] object-cover lg:mx-auto  "
         />
         <FiArrowLeftCircle
-          className="absolute left-12 top-1/2 translate-y-[-50%] text-emerald-500 bg-white px-2 rounded-full text-5xl cursor-pointer hover:-translate-x-2 transition-all duration-700"
+          className="absolute lg:left-12 left-3 top-1/2 lg:translate-y-[-100%]  text-emerald-500 lg:bg-white bg-[rgba(0,0,0,0.4)] lg:px-2 px-1  rounded-full lg:text-5xl text-4xl cursor-pointer hover:-translate-x-2 transition-all duration-700"
           onClick={handlePreviousPage}
         />
         <FiArrowRightCircle
           onClick={handleNextPage}
-          className="absolute right-12 top-1/2 translate-y-[-50%] text-emerald-500 bg-white px-2 rounded-full text-5xl cursor-pointer hover:translate-x-2 transition-all duration-700"
+          className="absolute lg:right-12 right-3 top-1/2 lg:translate-y-[-100%]  text-emerald-500 lg:bg-white bg-[rgba(0,0,0,0.4)] lg:px-2 px-1  rounded-full lg:text-5xl text-4xl cursor-pointer hover:translate-x-2 transition-all duration-700"
         />
-        <div className="flex items-center  mt-4 justify-evenly">
+        <div className="md:flex items-center  mt-4 justify-evenly hidden">
           {imgArr.map((img, index) => (
             <img
               key={img.id}
