dcefe3abdf6d1aef939af256bb82eeaf85a54c65
comptyadel96 <adoulation2007@hotmail.com>
2022-11-07 15:08:43 +0100

    about some changes

--- src/screens/Apropos.js
+++ src/screens/Apropos.js
@@ -13,22 +13,21 @@ function Apropos() {
           <p className="xl:text-4xl font-semibold">
             Historique de l'entreprise
           </p>
-          <p className="lg:mt-5 lg:text-xl leading-8">
+          <p className="lg:mt-5 lg:text-xl text-gray-600 leading-8">
             l'entreprise "Adara roto" a été créer en 2014, soit 14 ans aprés la
             création de l'entreprise sarl adara (créer en l'an 2000), notre
             entreprise a commancer officiellement la vente de citérnes en 2015.{" "}
-            <br />
           </p>
           <p className="xl:text-4xl font-semibold lg:mt-5">
             Partout en algérie
           </p>
-          <p className="lg:mt-5 lg:text-xl leading-7">
+          <p className="lg:mt-5 lg:text-xl text-gray-600 leading-7">
             notre succée dans le domaine est principalement dû à la qualité
             supérieur de nos citernes 3 couches conçus pour contenir eau et
-            acide
+            acide.
           </p>
           <p className="xl:text-4xl font-semibold lg:mt-5">Polyvalence</p>
-          <p className="lg:mt-5 lg:text-xl leading-7">
+          <p className="lg:mt-5 lg:text-xl text-gray-600 leading-7">
             l'entreprise sarl adara, et spécialisé dans la vente de citernes
             mais aussi pour la vente de matériels de plomberie et de tuyauterie
             PPRC (Polypropylène Random Copolymère). Son objectif est d’apporter
@@ -42,12 +41,44 @@ function Apropos() {
         </div>
       </div>
       {/* todo faq with background image and maps + image gallery */}
-      <div className="flex flex-col items-center  w-full lg:mt-20">
-        <p className="text-center lg:text-3xl font-semibold">
+      <div className="w-full lg:mt-20 lg:py-24 lg:p-5 bg-gray-100 mb-20">
+        <p className="text-center lg:text-4xl font-semibold mb-7">
           Questions fréquemment posées
         </p>
-        <div className="flex flex-wrap items-center"></div>
-        <Faq />
+        <div className="flex lg:flex-row flex-col flex-wrap items-center justify-evenly">
+          <Faq />
+          <Faq
+            question="Adara fait-elle la livraison ?"
+            reponse="si vous faite une commande importante, un grand nombre de citernes ou une citerne de 30.000 litres, nous vous assurerons une livraison gratuite "
+          />
+          <Faq
+            question="Toute température ?"
+            reponse="absolument, nos citérnes supportent une température de plus de 60 degrés celsius ainsi qu'une temperature relativement basse "
+          />
+          <Faq
+            question="seulement du 3 couches ?"
+            reponse="oui, adara roto ne produit que des citérnes 3 couches, vous ne pouvez donc pas faire une commande de plus ou de moins que 3 couches"
+          />
+          <Faq
+            question="Type de liquide supporter ?"
+            reponse="Notre usine produit des citérnes pour eau mais aussi pour les acides, avec les memes spécifications et les memes dimensions "
+          />
+        </div>
+      </div>
+      <div className="flex flex-col w-full bg-[#1d3557] lg:pb-24 lg:py-10">
+        <p className="lg:text-3xl text-center text-white">
+          Se déplacer vers notre emplacement
+        </p>
+        {/* google maps */}
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3204.331100942432!2d2.816552515602864!3d36.57023818830957!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x128f0b1bbab614c9%3A0xbe107c8812ea8125!2sAdara%20sarl!5e0!3m2!1sfr!2sdz!4v1666011523280!5m2!1sfr!2sdz"
+          style={{ border: 0 }}
+          className="mx-auto my-5 lg:w-1/3 lg:h-[20rem] border-none outline-none"
+          allowFullScreen={true}
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+          title="google maps adara roto vente citerne algérie"
+        />
       </div>
     </div>
   )
--- src/utils/Faq.js
+++ src/utils/Faq.js
@@ -1,15 +1,43 @@
-import React, { useState, useRef } from "react"
-
+import React, { useRef, useState } from "react"
+import { IoChevronDownSharp, IoChevronUpSharp } from "react-icons/io5"
 function Faq({
-  question = "puis-je ????",
-  reponse = "oui,vous pouvez si vous voulez",
+  question = "Les citernes sont ils assurées ?",
+  reponse = "oui, lors de votre achat vous bénéficerai d'une assurance de 6 mois",
 }) {
   const [isOpen, setIsOpen] = useState(false)
   const resRef = useRef(null)
+  const toggleRef = () => {
+    if (resRef.current.classList.contains("max-h-0")) {
+      resRef.current.classList.remove("max-h-0")
+      resRef.current.classList.add("max-h-[10rem]")
+      setIsOpen(!isOpen)
+    } else {
+      resRef.current.classList.add("max-h-0")
+      resRef.current.classList.remove("max-h-[10rem]")
+      setIsOpen(!isOpen)
+    }
+  }
   return (
-    <div className=" flex flex-col items-center lg:max-w-md relative bg-white lg:px-3 py-2">
-      <p> {question} </p>
-      <p>{reponse} </p>
+    <div
+      onClick={toggleRef}
+      className="my-3 lg:mx-5 overflow-hidden cursor-pointer flex flex-col items-center lg:max-w-md relative bg-white lg:px-3 lg:rounded-xl border border-gray-600 "
+    >
+      <div className="inline-flex items-center">
+        <p className="lg:text-2xl"> {question} </p>
+        {!isOpen ? (
+          <IoChevronDownSharp className="lg:text-xl text-gray-400 lg:ml-4 lg:mt-1" />
+        ) : (
+          <IoChevronUpSharp className="lg:text-xl text-gray-400 lg:ml-4 lg:mt-1" />
+        )}
+      </div>
+
+      <p
+        ref={resRef}
+        className="max-h-0 text-gray-500 text-center max-w-fit cursor-default lg:my-1 transition-all duration-1000 "
+        onClick={(e) => e.stopPropagation()}
+      >
+        {reponse}
+      </p>
     </div>
   )
 }
